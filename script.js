da6be3d3dd1972e7e97147063f244ef0c245e998
Damian Ramos <damianramos225@gmail.com>
2020-02-06 14:06:22 -0500

    fixed

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var showpassword = document.querySelector("#password")
 
 // Write password to the #password input
 function writePassword() {
     var password = generatePassword();
     var passwordText = document.querySelector("#password");
-    generatedPassword += passwordText[password];
     passwordText.value = password;
     console.log(password)
 }
@@ -16,15 +16,50 @@ var lowercaseArray = ["a", "b", "c", "e", "f", "g", "h"];
 var numberArray = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
 function generatePassword() {
-    var PassWordsize = prompt("How many character do you want in your password?")
-    var passwdSize = parseInt(PassWordsize)
+
+    // create empty array to hold all posible choices
+    var passwordOptions = [];
+
+    var passWordsize = prompt("How many characters do you want in your password?")
+    var lowerSize = confirm("Do you want lowercase characters in your password?")
+    var upperSize = confirm("Do you want uppercase characters in your password?")
+    var arraySpecial = confirm("Do you want special characters in your password?")
+    var arrayNumber = confirm("Do you want numbers in your password?")
+
+
+    if (lowerSize == true) {
+        // add lowercase array into passwordArray
+        passwordOptions = passwordOptions.concat(lowercaseArray)
+    }
+
+    if (upperSize == true) {
+
+        passwordOptions = passwordOptions.concat(UppercaseArray)
+    }
+
+    if (arraySpecial == true) {
+
+        passwordOptions = passwordOptions.concat(specialArray)
+    }
+
+    if (arrayNumber == true) {
+
+        passwordOptions = passwordOptions.concat(numberArray)
+    }
+
+
+
+
+
+
+    var passwdSize = parseInt(passWordsize)
     var generatedPassword = "";
 
 
     for (i = 0; i < passwdSize; i++) {
-        var passwordArray = [...specialArray, ...UppercaseArray, ...lowercaseArray, ...numberArray];
-        var index = Math.floor(Math.random() * passwordArray.length)
-        generatedPassword += passwordArray[index]
+        //var passwordArray = [...specialArray, ...UppercaseArray, ...lowercaseArray, ...numberArray];
+        var index = Math.floor(Math.random() * passwordOptions.length)
+        generatedPassword += passwordOptions[index]
         console.log(generatedPassword)
     }
 
